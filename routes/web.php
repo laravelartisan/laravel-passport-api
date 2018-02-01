11da3511026e1f5cdccaaefd66591fe8bb57eb48
Nazim <Nazim>
2018-02-01 19:32:28 +0600

    authorization done

--- routes/web.php
+++ routes/web.php
@@ -12,41 +12,15 @@
 */
 
 Route::get('/', function () {
+//    dd(444);
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+/*Route::get('/test', function () {
+    return 'Client Created';
+});*/
 
-Route::get('/redirect', function () {
-    $query = http_build_query([
-        'client_id'     => '1',
-        'redirect_uri'  => 'http://localhost:8000/oauth/callback',
-        'response_type' => 'code',
-        'scope'         => '',
-    ]);
 
-    return redirect('http://localhost:8000/oauth/authorize?' . $query);
-});
-
-Route::get('/oauth/callback', function () {
-
-    $http = new GuzzleHttp\Client;
-
-    if (request('code')) {
-        $response = $http->post('http://localhost:8000/oauth/token', [
-            'form_params' => [
-                'grant_type'    => 'authorization_code',
-                'client_id'     => '1',
-                'client_secret' => 'zfqGslYXzUcsieFINy4NECHolIUQ67LxgHnTLj8N',
-                'redirect_uri'  => 'http://localhost:8000/oauth/callback',
-                'code'          => request('code'),
-            ],
-        ]);
-
-        return json_decode((string)$response->getBody(), TRUE);
-    } else {
-        return response()->json(['error' => request('error')]);
-    }
-});
